de6e26a3eb85d55621d001e22c7aea3b4eaa6f4a
Philip <ptnapoleon@gmail.com>
2013-02-11 21:18:57 -0500

    changed variable names. started options

--- Project_Files/UI.java
+++ Project_Files/UI.java
@@ -10,15 +10,16 @@ public class UI
 		//FileServer is named "FilePile" and is on 4321
 
 		System.out.println("Attempting to connect to GroupServer.\n");
-		GroupClient user = new GroupClient();
+		GroupClient gUser = new GroupClient();
 		user.connect(null, 8766);
 		
 		System.out.println("Please enter a username.");
 		Scanner in = new Scanner(System.in);
 
 		String username = in.nextLine();
-		UserToken token = user.getToken(username);
-
+		UserToken token = gUser.getToken(username);
+		//confirmed that this token is correct
+		System.out.println("What would you like to do now?");//Queries the user
 		
 	}
 }
